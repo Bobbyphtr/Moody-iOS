75e3213510bd38dd13865e81c1da78725c7d4815
Bobbyphtr <bobby.pehtrus@traveloka.com>
2024-01-03 22:42:09 +0700

    Initial Commit

---
+++ Moody/MoodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodyApp.swift
+//  Moody
+//
+//  Created by Bobby Pehtrus on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MoodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
